1fd0fd1fc5d566ce3fd5f32efddb9bd8a6a98667
sasukeuchiwa666 <zakharovda95@gmail.com>
2022-02-07 14:55:15 +0300

    М4 З2

---
+++ 04-vue-cli/02-TheToaster/components/UiContainer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <slot>Тут должен быть текст</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UiContainer',
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+}
+</style>
